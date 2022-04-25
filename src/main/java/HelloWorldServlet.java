d1b17971095a04ce62782d318d040de79a65c8b8
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-25 16:11:21 -0500

    feat: servlets-exercise through first bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,17 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    String name = req.getParameter("name");
+    res.getWriter().println("Howdy, " + req.getParameter("name") + "!");
     }
 }
+
+
