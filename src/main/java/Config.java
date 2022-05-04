48eb8e2d4d0278f0df6bc507f79f17d3d19c5aa6
Lorne Cousins <lorne.cousins88@gmail.com>
2022-05-04 10:44:59 -0500

    refactor of jdbc exercise

---
+++ src/main/java/Config.java
@@ -0,0 +1,15 @@
+public class Config {
+    public String getUrl(){
+        return "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+
+    public String getUsername(){
+        return "root";
+    }
+
+    public String getPassword(){
+        return "codeup-1";
+    }
+}
+
+
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,49 @@
+import com.mysql.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection;
+
+    public MySQLAdsDao() {
+        try {
+            Config config = new Config();
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("Select * FROM ");
+            while (rs.next()) {
+                Ad ad = new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                );
+                ads.add(ad);
+            }
+            } catch (SQLException sqle) {
+                throw new RuntimeException("error connecting to db", sqle);
+            }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
